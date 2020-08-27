24ed0afa25f2145b1c2e76297ddf70f5f7ff5078
Nicula Iulian-Stefan <nicula_iulian_stefan@yahoo.com>
2020-08-28 01:58:04 +0300

    Add files via upload

---
+++ Assets/Scripts/PacmanMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Net;
+using UnityEngine;
+
+public class PacmanMove : MonoBehaviour
+{
+    public float speed = 0.4f;
+    public static int size = 30;
+    public bool [,]isMaze = new bool[size, size];
+    public int[,] maze = new int[size, size];
+    Vector2 dest = Vector2.zero;
+    PacmanMove()
+    {
+        int i, j;
+        for (i = 2; i <= 27; i++)
+        {
+            for (j = 2; j <= 30; j++)
+            {
+            }
+        }
+    }
+    bool valid(Vector2 dir)
+    {
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+        return (hit.collider == GetComponent<Collider2D>());
+    }
+
+    void Start()
+    {
+        dest = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+        GetComponent<Rigidbody2D>().MovePosition(p);
+        if ((Vector2)transform.position == dest)
+        {
+            if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
+                dest = (Vector2)transform.position + Vector2.up;
+            if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
+                dest = (Vector2)transform.position + Vector2.right;
+            if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
+                dest = (Vector2)transform.position - Vector2.up;
+            if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
+                dest = (Vector2)transform.position - Vector2.right;
+        }
+
+        Vector2 dir = dest - (Vector2)transform.position;
+        GetComponent<Animator>().SetFloat("DirX", dir.x);
+        GetComponent<Animator>().SetFloat("DirY", dir.y);
+    }
+}
